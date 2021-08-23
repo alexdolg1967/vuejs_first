6606c50752a06c20a9a9c8a15def0f05037c7255
alexdolg1967 <alexdolg1967@mail.ru>
2021-08-23 08:04:29 +0300

    install express

--- pages/users.vue
+++
@@ -1,6 +0,0 @@
-<template>
-	<div>
-		<h2>Обертка для страниц users</h2>
-		<nuxt-child />
-	</div>
-</template>
--- pages/users/create.vue
+++
@@ -1,5 +0,0 @@
-<template>
-	<div>
-		<h2>Создание пользователя</h2>
-	</div>
-</template>
